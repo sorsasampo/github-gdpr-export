bff47c985d1cfe082dec1bd7fa029d5fe297ab96
Sampo Sorsa <sorsasampo@protonmail.com>
2018-05-27 00:00:00 +0000

    Implement download command

--- github-gdpr-export.py
+++ github-gdpr-export.py
@@ -5,6 +5,19 @@ import argparse
 import requests
 
 
+def download(args):
+    headers = {
+        'Accept': 'application/vnd.github.wyandotte-preview+json',
+        'Authorization': 'token %s' % args.token,
+    }
+    r = requests.get('https://api.github.com/user/migrations/%d/archive' % args.id, headers=headers, stream=True)
+    r.raise_for_status()
+    print('Downloading "%s" into "%s"' % (r.url, args.filename))
+    with open(args.filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=4096):
+            f.write(chunk)
+
+
 def list_migrations(args):
     headers = {
         'Accept': 'application/vnd.github.wyandotte-preview+json',
@@ -27,6 +40,11 @@ def list_user_repos(args):
 parser = argparse.ArgumentParser(description='Export GitHub projects using migration API')
 parser.add_argument('--token', help='GitHub personal access token\nhttps://github.com/settings/tokens')
 subparsers = parser.add_subparsers(help='command')
+
+parser_download = subparsers.add_parser('download', description='Fetch URL for downloading the migration archive')
+parser_download.add_argument('id', type=int, help='Migration id')
+parser_download.add_argument('filename', help='Filename to save as')
+parser_download.set_defaults(func=download)
 
 parser_list = subparsers.add_parser('list', description='List user migrations')
 parser_list.set_defaults(func=list_migrations)
